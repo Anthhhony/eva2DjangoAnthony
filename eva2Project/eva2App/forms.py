15a81212ae4765b0ff7a0eeed298444d6a2ce9d4
Anthhhony <maldonadocampusanoanthy@gmail.com>
2024-10-28 12:24:16 -0300

    commitFinal

--- eva2Project/eva2App/forms.py
+++ eva2Project/eva2App/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from eva2App.models import DBproyecto
 
+
 class formulario(forms.Form):
     rut = forms.CharField(max_length=12)
     nombre = forms.CharField(max_length=30)
